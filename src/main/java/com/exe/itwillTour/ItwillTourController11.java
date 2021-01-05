746980407c764de2c92ce3b971fd0442a36bd011
greetoss <gusgh483@naver.com>
2021-01-05 19:00:05 +0900

    yeonji brench 인수합병

--- src/main/java/com/exe/itwillTour/ItwillTourController11.java
+++ src/main/java/com/exe/itwillTour/ItwillTourController11.java
@@ -1,9 +1,14 @@
 package com.exe.itwillTour;
 
+import java.io.File;
+import java.security.Provider.Service;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,7 +31,7 @@ public class ItwillTourController11 {
 
 	@RequestMapping("/itwillTour11")
 	public String itwillTour11(HttpSession session, Model model) {
-
+		
 		//test
 		session.setAttribute("userID", "apple");
 		//test
@@ -38,8 +43,72 @@ public class ItwillTourController11 {
 	}
 
 	@ResponseBody
+	@PostMapping("fileUpload.action")
+	public String fileUploadTestAction(MultipartHttpServletRequest multi) {
+		
+		String uploadPath = multi.getSession().getServletContext().getRealPath("/").replace(".metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\", "")+"src\\main\\webapp\\resources\\upload\\";
+
+		Map<String, String> paramMap = new HashMap<String, String>();
+		paramMap.put("user_name", multi.getParameter("user_name"));
+		paramMap.put("user_tel", multi.getParameter("user_tel"));
+		paramMap.put("user_email", multi.getParameter("user_email"));
+		paramMap.put("category", multi.getParameter("category"));
+		paramMap.put("subject", multi.getParameter("subject"));
+		paramMap.put("content", multi.getParameter("content"));
+		paramMap.put("answerMethod", multi.getParameter("answerMethod"));
+		
+		long time = System.currentTimeMillis();
+		
+		List<MultipartFile> fileList = new ArrayList<MultipartFile>();
+		if(!multi.getFile("file1").getOriginalFilename().equals("")) {
+			fileList.add(multi.getFile("file1"));
+			}
+		if(!multi.getFile("file2").getOriginalFilename().equals("")) {
+			fileList.add(multi.getFile("file2"));
+		}
+		if(!multi.getFile("file3").getOriginalFilename().equals("")) {
+			fileList.add(multi.getFile("file3"));		
+		}
+		
+		System.out.println("파일 개수 : "+fileList.size());
+		
+		File fileDir = new File(uploadPath);
+		if(!fileDir.exists()) fileDir.mkdirs();
+		
+		if(fileList.size()!=0) {
+			time = System.currentTimeMillis();
+			
+			int cnt = 0;
+			String originFileName, saveFileName;
+			for (MultipartFile mf : fileList) {
+				originFileName = mf.getOriginalFilename();
+				saveFileName = String.format("%d_%s",time,originFileName);
+				
+				if(saveFileName.getBytes().length>100) {
+					//여기 해야 함
+				}
+				
+				
+				paramMap.put("file"+(++cnt), saveFileName);
+				try {
+					mf.transferTo(new File(uploadPath, saveFileName));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}		
+		}
+		
+		serv.submitInquiry(paramMap);
+		
+		return "itwillTour10to19/fileUploadTest";
+	}	
+	
+	
+/*	
+	@ResponseBody
 	@PostMapping("submitInquiry.action")
-	public String submitInquiry(/*@RequestParam Map<String, String> params*/MultipartHttpServletRequest multi) {
+	public String submitInquiry(MultipartHttpServletRequest multi) {
+		
 		
 		multi.getParameter("category");
 		multi.getParameter("user_name");
@@ -47,16 +116,16 @@ public class ItwillTourController11 {
 		multi.getParameter("user_email");
 		multi.getParameter("subject");
 		multi.getParameter("content");
-
+		
 		List<MultipartFile> fileList = new ArrayList<MultipartFile>();
 		fileList.add(multi.getFile("file1"));
 		fileList.add(multi.getFile("file2"));
 		fileList.add(multi.getFile("file3"));
-
+		
 		//int result = serv.submitInquiry(params);
 		return "itwillTour10to19/itwillTour11";
 	}	
-
+*/
 	/* @RequestParam으로 데이터 받는 방법
  	@PostMapping("submitInquiry.action")
 	public String submitInquiry(
--- src/main/java/com/exe/itwillTourDAO/ItwillTourDAO11.java
+++ src/main/java/com/exe/itwillTourDAO/ItwillTourDAO11.java
@@ -2,8 +2,10 @@ package com.exe.itwillTourDAO;
 
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
 import com.zaxxer.hikari.HikariConfig;
 
 @Repository
